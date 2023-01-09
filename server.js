39073444783866f40adc813341b3b19c067f5c11
Lê Trung Sơn <sonlt98@vnpt.vn>
2023-01-09 17:06:24 +0700

    commit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ postgres.connect(function (err) {
 
 let binance;
 let ping = new Monitor({
-    website: 'https://con-bot-dot.herokuapp.com',
+    website: 'https://con-bot-ngu.onrender.com',
     interval: 20 // minutes
 });
 
