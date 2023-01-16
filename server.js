c02aa4e18f36726aea77e5a5b5cec87cee199f97
Lê Trung Sơn <sonlt98@vnpt.vn>
2023-01-16 10:16:01 +0700

    commit

--- server.js
+++ server.js
@@ -266,9 +266,9 @@ io.on('connect', function (socket) {
         });
     });
 
-    socket.on('disconnect', function () {
-        console.log(socket.id + " Da ngat ket noi!");
-    });
+//    socket.on('disconnect', function () {
+//        console.log(socket.id + " Da ngat ket noi!");
+//    });
 
 });
 
