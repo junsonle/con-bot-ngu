d5533adbc51032fbba5c03daf650a390716b972b
Lê Trung Sơn <sonlt98@vnpt.vn>
2023-02-07 13:48:26 +0700

    commit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ postgres.connect(function (err) {
 
 let binance;
 let ping = new Monitor({
-    website: 'https://con-bot-ngu-zjln.onrender.com',
+    website: 'https://con-bot-dot.onrender.com',
     interval: 10 // minutes
 });
 
